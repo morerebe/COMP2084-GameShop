76b57e804c69d2896dd2da226fce5c6a8354c1ab
morerebe <23equalsodd!>
2020-09-28 13:33:13 -0400

    add all files

---
+++ Dropbox/Georgian/Semester 3/Server-Side-Scripting/Week2/COMP2084GetMeAGame/COMP2084-GameShop/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP2084_GameShop.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
